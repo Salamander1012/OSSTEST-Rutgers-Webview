87ee04823cf357bfd97095d64d94a1dc18862d4b
Salman Fakhri <salmanfakhri@nbp-101-220.nbp.ruw.rutgers.edu>
2017-11-15 15:25:11 -0500

    add pods SwiftyJson

---
+++ Pods/Target Support Files/Pods-interviewTest/Pods-interviewTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_interviewTest : NSObject
+@end
+@implementation PodsDummy_Pods_interviewTest
+@end
--- interviewTest/ViewController.swift
+++ interviewTest/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Foundation
+import SwiftyJSON
 
 struct MenuListItem {
     let title: String
